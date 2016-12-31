fde2a53d05abab7cfc919ebc26067ff579b05473
Mahmood Ali <Mahmood Ali@DESKTOP-N912126>
2016-12-31 19:46:13 +0300

    Add Recipe php file

--- include/add_recipe_function.php
+++ include/add_recipe_function.php
@@ -16,13 +16,97 @@ class add_recipe_function {
     private $conn;
     
     public function __construct() {
-        require_once 'include/add_recipe_function';
+        require_once 'include/db_connect.php';
         $db = new db_connect();
-        $this->conn=$db->connec();
+        $this->conn=$db->connect();
     }
     
-    public function InsertRecipe($user_fk,$recipe_name,$cooking_time,$serving,$ingredients,$recipe_step){
+    public function insertRecipe($user_fk,$recipe_name,$cooking_time,$serving){
+        $stmt=$this->conn->prepare("insert into recipes (user_fk,title,"
+                . "cooking_time,serving) values(?,?,?,?) ");
+        $stmt->bind_param("isii",$user_fk,$recipe_name,$cooking_time,$serving);
+        if($stmt->execute()){
+           
+            $stmt=$this->conn->prepare("select recipe_id from recipes where "
+                    . "user_fk=? order by recipe_id DESC" );
+            $stmt->bind_param("i",$user_fk);
+            $stmt->execute();
+            
+            $stmt->bind_result($recipe_id);
+            $stmt->fetch();
+            
+            $stmt->close();
+            
+            return $recipe_id;
+            
+        }else{
+            echo "false";
+            return false;
+        }
         
+    }
+    
+    public function insertRecipe_categories($recipe_id,$categories){
+        
+        $stmt=$this->conn->prepare("insert into recipe_category "
+                . "(recipe_fk,category_fk) values (? ,?)");
+        
+        $mCatrgories = explode(",", $categories);
+        $isSuccess=0;
+        
+        foreach ($mCatrgories as $catrgory) {
+            $stmt->bind_param("ii",$recipe_id,$catrgory);
+            $isSuccess=$stmt->execute();
+        }
+        
+        return $isSuccess;
+       
+    }
+    
+    public function insertRecipe_image($recipe_id,$img){
+        $stmt=$this->conn->prepare("insert into recipes_img (recipe_fk,img_url)"
+                . " values(?,?)");
+        $stmt->bind_param("is",$recipe_id,$img);
+        
+        if($stmt->execute()){
+            $stmt->close();
+            return true;
+        }else{
+            $stmt->close();
+            echo  "false";
+            return false;
+        }
+    }
+    
+    public function insertRecipe_Ingredients($recipe_id,$ingredients){
+        $stmt=$this->conn->prepare("insert into ingredients (recipe_fk,ingredient)"
+                . " values(?,?)");
+        $stmt->bind_param("is",$recipe_id,$ingredients);
+        
+        if($stmt->execute()){
+            $stmt->close();
+            return true;
+        }else{
+            $stmt->close();
+            echo  "false";
+            return false;
+        }
+    }
+    
+    
+    public function insertRecipe_steps($recipe_id,$recipe_steps){
+        $stmt=$this->conn->prepare("insert into recipe_steps (recipe_fk,instructions)"
+                . " values(?,?)");
+        $stmt->bind_param("is",$recipe_id,$recipe_steps);
+        
+        if($stmt->execute()){
+            $stmt->close();
+            return true;
+        }else{
+            $stmt->close();
+            echo  "false";
+            return false;
+        }
     }
     
     
